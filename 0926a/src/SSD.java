bd12d5031d62d4a408304fbd68b2a39033ac82d5
yo <yo@192.168.0.2>
2023-09-26 16:20:41 +0900

    1212

--- 0926a/src/SSD.java
+++ 0926a/src/SSD.java
@@ -1,5 +1,22 @@
 public class SSD {
     public static void main(String[] args) {
-        System.out.println("안녕");
+        int sum = 0;
+
+        for (int num = 2; num <= 1000; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                sum += num;
+            }
+        }
+
+        System.out.println("1부터 1000 사이의 소수의 합은: " + sum);
     }
 }
