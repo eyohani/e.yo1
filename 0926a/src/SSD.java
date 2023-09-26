607e8cc5c5972e8c1313dc7bfea860c9713fd4fc
yo <yo@192.168.0.2>
2023-09-26 16:24:52 +0900

    real

--- 0926a/src/SSD.java
+++ 0926a/src/SSD.java
@@ -4,21 +4,16 @@ public class SSD {
 
         for (int x = 2; x <= 1000; x++) {
             boolean isPrime = true;
-
-            // 2부터 해당 숫자의 제곱근까지 나누어 소수를 판별합니다.
             for (int i = 2; i <= Math.sqrt(x); i++) {
                 if (x % i == 0) {
                     isPrime = false;
                     break;
                 }
             }
-
-            // 소수인 경우 합산합니다.
             if (isPrime) {
                 sum += x;
             }
         }
-
         System.out.println("1부터 1000 사이의 소수의 합은: " + sum);
     }
 }
