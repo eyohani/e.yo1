383be1f67c490ca21d0739014066773346d5e207
yo <yo@192.168.0.2>
2023-09-26 16:28:30 +0900

    real

--- 0926a/src/SSD.java
+++ 0926a/src/SSD.java
@@ -1,19 +1,26 @@
 public class SSD {
     public static void main(String[] args) {
-        int sum = 0;
-
-        for (int x = 2; x <= 1000; x++) {
-            boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(x); i++) {
-                if (x % i == 0) {
-                    isPrime = false;
-                    break;
+        int max = 1000;
+        boolean[] a = new boolean[max + 1]; // 각 숫자가 소수인지 여부를 저장하는 배열
+        for (int i = 2; i <= max; i++) {
+            a[i] = true;
+        }
+        for (int p = 2; p * p <= max; p++) {
+            if (a[p]) {
+                for (int i = p * p; i <= max; i += p) {
+                    a[i] = false;
                 }
             }
-            if (isPrime) {
-                sum += x;
+        }
+        int sum = 0;
+        System.out.println("1부터 1000 사이의 소수는:");
+        for (int i = 2; i <= max; i++) {
+            if (a[i]) {
+                System.out.print(i + " ");
+                sum += i;
             }
         }
-        System.out.println("1부터 1000 사이의 소수의 합: " + sum);
+
+        System.out.println("\n1부터 1000 사이의 소수의 합은: " + sum);
     }
 }
